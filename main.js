7fef1f1ee498c8475f56dcecbe4fb7e808ef1250
Jhon <jhonatanjf0408@gmail.com>
2024-09-18 12:26:46 -0300

    Html & CSS

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const somar = (num1, num2) => num1 + num2
+const somar = (num1, num2) => num1 + num2;
 
-console.log(somar(10, 22))
+document.getElementById('somar').addEventListener('click', function () {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+
+    const resultado = somar(num1, num2);
+
+    document.getElementById('resultado').textContent = `Resultado: ${resultado}`;
+});
